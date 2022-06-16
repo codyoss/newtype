cb3352a52fd14d0e46cbad0ac353aaf8e6c5e1e8
Cody Oss <codyoss@google.com>
2022-06-16 10:31:06 -0500

    add types, update sigs

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/codyoss/newtype
 
 go 1.18
-
-require github.com/codyoss/orgtype v0.0.0-20220616144937-99f80a6ccbbc
--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package newtype
 
-import "github.com/codyoss/orgtype"
-
 func NewSomething() *Something {
 	return &Something{}
 }
 
 type Something struct{}
 
-func (s *Something) AMethod(_ orgtype.AType) orgtype.AnotherType {
-	return orgtype.AnotherType{}
+func (s *Something) AMethod(_ AType) AnotherType {
+	return AnotherType{}
+}
+
+var AVar bool
+
+const AConst = 1
+
+func AFunc() {}
+
+func AnotherFunc(*AType) *AnotherType {
+	return nil
+}
+
+type AType struct {
+	AField bool
+}
+
+type AnotherType struct {
+	AField int
+}
+
+type AInterface interface {
+	AMethod(*AType) AnotherType
 }
