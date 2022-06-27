4bf4add76d7963b8a3a89e6d33ced5469a03b501
Cody Oss <codyoss@google.com>
2022-06-27 11:44:35 -0500

    add types

--- main.go
+++ main.go
@@ -5,8 +5,31 @@ import "github.com/codyoss/orgtype"
 func NewSomething() *Something {
 	return &Something{}
 }
+
 type Something struct{}
 
 func (s *Something) AMethod(_ *orgtype.AType) orgtype.AnotherType {
 	return orgtype.AnotherType{}
+}
+
+var AVar bool
+
+const AConst = 1
+
+func AFunc() {}
+
+func AnotherFunc(*AType) *AnotherType {
+	return nil
+}
+
+type AType struct {
+	AField bool
+}
+
+type AnotherType struct {
+	AField int
+}
+
+type AInterface interface {
+	AMethod(*AType) AnotherType
 }
