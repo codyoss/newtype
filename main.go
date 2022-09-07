5b5ae49cf23293e5cefdfce790bf144729a721bd
Cody Oss <codyoss@google.com>
2022-09-07 15:22:05 -0500

    rewind

--- main.go
+++ main.go
@@ -1,33 +1,13 @@
 package newtype
 
+import "github.com/codyoss/orgtype"
+
 func NewSomething() *Something {
 	return &Something{}
 }
 
 type Something struct{}
 
-func (s *Something) AMethod(_ *AType) AnotherType {
-	return AnotherType{}
-}
-
-var AVar bool
-
-const AConst = 1
-
-func AFunc() {}
-
-func AnotherFunc(*AType) *AnotherType {
-	return nil
-}
-
-type AType struct {
-	AField bool
-}
-
-type AnotherType struct {
-	AField int
-}
-
-type AInterface interface {
-	AMethod(*AType) AnotherType
+func (s *Something) AMethod(_ *orgtype.AType) orgtype.AnotherType {
+	return orgtype.AnotherType{}
 }
